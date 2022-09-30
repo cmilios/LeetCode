2f1a3fcd46af998dc7b8cad4552f141b1451c9e3
Chris Milios <cmilios@hotmail.com>
2022-09-30 11:15:24 +0300

    Longest Substring question completed

---
+++ LongestSubstrWithoutRepeatingChars/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LongestSubstrWithoutRepeatingChars{
+    
+    public class LongestSubstrWithoutRepeatingChars
+    {
+        public static void Main(string[] args)
+        {
+            string s = "aab";
+            int result = LengthOfLongestSubstring(s);
+            Console.WriteLine(result);
+        }
+        
+        public static int LengthOfLongestSubstring(string s)
+        {
+            var biggestStr = "";
+            var Index = 0;
+            var Max = 0;
+
+            for(int i=0; i<s.Length; i++)
+            {
+                if (biggestStr.Contains(s[i]))
+                {
+                    biggestStr = "";
+                    i = Index;
+                    Index++;
+                }
+                else
+                {
+                    biggestStr += s[i];
+                    if (Max < biggestStr.Length) Max = biggestStr.Length;
+                }
+            }
+
+            return Max;
+            
+        }
+    }
+
+
+}
